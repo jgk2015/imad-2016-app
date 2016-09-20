fba4df215c5c2173ccef499576c84e0da9088ee4
jgk2015 <jgk000@hotmail.com>
2016-09-20 17:43:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articles.article-one));
+  res.send(createTemplate(article-one));
 });
 
 app.get('/article-two', function (req, res) {
