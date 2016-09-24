3d7f414105621924100104848f9ba26f5672657b
jgk2015 <jgk000@hotmail.com>
2016-09-24 13:24:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,13 @@ var element = document.getElementById('main-text');
 element.innerText = "New value set by js code";
 
 var img = document.getElementById('madi');
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.maginLeft = marginLeft + 'px';
+}
 
 img.onclick = function() {
+    var interval = setInterval(moveRight, 100);
     img.style.marginLeft = '100px';
     console.log('ON CLICK EVENT');
 };
