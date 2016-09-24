b4341591ff9ce11505a70af59d1b3c9868d41d0b
jgk2015 <jgk000@hotmail.com>
2016-09-24 13:08:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,11 @@
 console.log('Loaded!');
 
 var element = document.getElementById('main-text');
-element.innerHTML = "New value set by js code";
+
+element.innerText = "New value set by js code";
 
 var img = document.getElementById('madi');
 
 img.onclick = function() {
-    img.style.margin-left = '100px';
+    img.style.marginLeft = '100px';
 };
