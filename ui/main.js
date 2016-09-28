ea3d7821ae897ef59aa8fe69f673573f3ca3676f
jgk2015 <jgk000@hotmail.com>
2016-09-28 19:00:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,19 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
+
 button.onClick(){
     
+    var request = new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span = document.getElementByID('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if (request.readystate === XMLHttpRequest.DONE){
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementByID('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://jgk2015.imad.hasura-app.io/counter');
 };
